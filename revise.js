62c97b5e81960c26fb186df4b70044b7b7a0c03f
preenakumari <preenakumari6@gmail.com>
2024-01-25 01:31:48 +0500

    work

---
+++ revise.js
@@ -0,0 +1,87 @@
+// var a = "Prena"
+// var b = "kashish"
+// //Kashish give pen to prena
+// console.log(b + " give pen to " + a)//concat
+//"" string
+//pasia watho ammi kha check karo ammi agar pasia 5000 kha matha dina alert show wow agar gat dena ta oh
+//greater,>|less,<|equal,===
+// var pasia=prompt("ammi muja pasia do")
+// if(pasia>5000){
+//     var ask=prompt("cha asee car ma shopping la halo")
+//     if(ask==="yes"){
+//         alert("wow")
+//     }
+//     else{
+//         alert("oh")
+//     }
+// }else{
+//     alert("oh")
+// }
+
+// var num1=Number(prompt("Enter your first number"))
+// var num2=Number(prompt("Enter your second number"))
+// var sign=prompt("enter operator sign")
+// if(sign==='+'){
+//     console.log(num1+num2)
+// }
+// else if(sign==='-'){
+//     console.log(num1-num2)
+// }
+// else if(sign==='*'){
+//     console.log(num1*num2)
+// }
+// else if(sign==='/'){
+//     console.log(num1/num2)
+// }
+// else{
+//     alert("wrong sign")
+// }
+var k="komal"
+var s="sheela"
+console.log(s+ "you are coming my home" + k)
+//prompt kashish chose karo rock,paper,scissor
+//agar kasish na paper ja barbar hu 
+//prompt  prena chose karo rock,paper,scissor
+//agar prena chose kayo scissor ta prena win alert
+//kashish win
+var i=prompt("kashish chose karoo, rock, paper, scissor")
+if(i==="paper"){
+    var e=prompt("preena chose kayo ,rock ,paper,scissor")
+if(e==="scissor"){
+    alert( " preena win" )
+}else{
+    if(i===e){
+        alert("draw")
+    }else
+alert("kashish win")
+}
+}
+else if(i==="rock"){
+    var e=prompt("preena chose kayo ,rock ,paper,scissor")
+if(e==="paper"){
+    alert( " preena win" )
+}else{
+    if(i===e){
+        alert("draw")
+    }else
+alert("kashish win")
+}
+}
+else if(i==="scissor"){
+    var e=prompt("preena chose kayo ,rock ,paper,scissor")
+if(e==="rock"){
+    alert( " preena win" )
+}else{
+    if(i===e){
+        alert("draw")
+    }else
+alert("kashish win")
+}
+}
+else{
+    alert("apna wrong sign")
+}
+
+
+
+
